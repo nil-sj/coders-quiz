f857a90499af09a9cacb82f4048da42f5fe476c3
Niladri Sekhar Jyoti <niladri123@gmail.com>
2024-08-16 17:16:38 -0400

    added new base api removed/

--- quiz-ui/src/services/apiConfig.js
+++ quiz-ui/src/services/apiConfig.js
@@ -1,3 +1,3 @@
-const BASE_API_URL = 'https://quiz-backend-production-b338.up.railway.app/';
+const BASE_API_URL = 'https://quiz-backend-production-b338.up.railway.app';
 
 export default BASE_API_URL;
