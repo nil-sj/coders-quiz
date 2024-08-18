a5c85b9c0908b2f8147dc18e075d8f84943377e5
Niladri Sekhar Jyoti <niladri123@gmail.com>
2024-08-17 23:22:21 -0400

    added CORS and app password

--- quiz-api/src/main/java/com/codersquiz/quiz_api/QuizApiApplication.java
+++ quiz-api/src/main/java/com/codersquiz/quiz_api/QuizApiApplication.java
@@ -21,7 +21,7 @@ public class QuizApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000", "https://liftoff-curious.vercel.app")
+						.allowedOrigins("http://localhost:3000", "https://liftoff-curious.vercel.app", "https://codersquizui.netlify.app", "https://quiz.codenil.online")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
